da0e784335273fd9eec763779eca3c62e1376478
Nicholas <Morchain>
2019-12-05 14:57:42 -0500

    mp3

--- oral-history/popcorn-data.js
+++ oral-history/popcorn-data.js
@@ -3,12 +3,32 @@ var loadPopcorn = function () {
 var pop = Popcorn( "#media" );
  pop.image({
  // seconds
-start: 2,
+start: 1,
 // seconds
-end: 15,
+end: 4,
 href: "https://www.shopify.com",
-src: "https://cdn.shopify.com/s/files/1/0815/6851/articles/tracy168_widlstyle.jpg?v=1534438002",
-text: "GRAFITTI",
+src: "https://wp.zillowstatic.com/streeteasy/2/GettyImages-626346156-resized-f3c71f.jpg",
 target: "popcorn-container"
 });
+var pop = Popcorn( "#media" );
+pop.image({
+    // seconds 
+    start: 18,
+    // seconds
+    end: 33,
+    src: "https://images-na.ssl-images-amazon.com/images/I/51rQ%2BeXOq0L._SX466_.jpg",
+    target: "popcorn-container"
+});
+var pop = Popcorn( "#media" );
+pop.openmap( {
+  start: 5,
+  end: 20,
+  type: "ROADMAP",
+  target: "map",
+  lat: 40.847055,
+  lng: -73.924486,
+  zoom: "10"
+});
+
+
 };
